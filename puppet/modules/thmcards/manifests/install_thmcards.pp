94d46c59e86aac9e15c1caab2ab78df7683cca8e
Pierre Rossel <nospam.pierre.rossel@hesge.ch>
2014-07-16 12:37:50 +0200

    add default badges

--- puppet/modules/thmcards/manifests/install_thmcards.pp
+++ puppet/modules/thmcards/manifests/install_thmcards.pp
@@ -4,16 +4,19 @@ define install_thmcards (
 	exec { "install npm":
 	  command => "/usr/bin/apt-get install -q -y npm"
 	}
+	->
 	exec { "install thmcards":
 	  cwd => "/vagrant/thmcards",
 	  command => "/usr/bin/npm install"
 	}
+	->
 	exec { "create views":
 	  cwd => "/vagrant/thmcards",
 	  command => "/usr/bin/python createviews.py"
 	}
-	# exec { "run app.js":
-	#   path => "/vagrant/thmcards",
-	#   command => "/usr/bin/nodejs app.js"
-	# }
+	->
+	exec { "default badges":
+	  cwd => "/vagrant/thmcards",
+	  command => "/usr/bin/curl -d @/vagrant/thmcards/couchviews/default_badges.design -X POST http://127.0.0.1:5984/thmcards/_bulk_docs -H \"Content-Type: application/json\"",
+	}
 }
